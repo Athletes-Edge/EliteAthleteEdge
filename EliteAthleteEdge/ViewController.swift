ec360375def6215468634756ab79006bf7e320b3
johnbuzzware <johnbuzzware@gmail.com>
2025-02-03 20:28:11 +0500

    Initial Commit

---
+++ EliteAthleteEdge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EliteAthleteEdge
+//
+//  Created by ali john on 03/02/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ EliteAthleteEdgeUITests/EliteAthleteEdgeUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  EliteAthleteEdgeUITestsLaunchTests.swift
+//  EliteAthleteEdgeUITests
+//
+//  Created by ali john on 03/02/2025.
+//
+
+import XCTest
+
+final class EliteAthleteEdgeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
